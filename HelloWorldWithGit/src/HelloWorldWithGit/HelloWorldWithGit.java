c89e19097e0db32444be464c7e3f3f6adf0dae5d
Guokai Shi <shi00057@algonquinlive.com>
2024-03-14 21:59:48 -0400

    New project HelloWorldWithGit
    Updata main method
    
    Signed off by Guokai Shi <shi00057@algonquinlive.com>

---
+++ HelloWorldWithGit/src/HelloWorldWithGit/HelloWorldWithGit.java
@@ -0,0 +1,10 @@
+package HelloWorldWithGit;
+
+public class HelloWorldWithGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World with Git.");
+	}
+
+}
